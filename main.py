78a2ff75d8f08037b4a9ed754bde4fbe7470fd45
Bogdan Reznikov <bogdanreznikov04@gmail.com>
2023-02-20 14:24:28 +0100

    CameCase, added dataframe for ePg and eSg also code is more  commented

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 import datetime
 import time
+import pandas
 import re
-month_dictionary = {
-    "JAN": 1,
-    "FEB": 2,
-    "MAR": 3,
-    "APR": 4,
-    "MAY": 5,
-    "JUN": 6,
-    "JUL": 7,
-    "AUG": 8,
-    "SEP": 9,
-    "OCT": 10,
-    "NOV": 11,
-    "DEC": 12}
-
 
 class Parser:
 
@@ -27,48 +14,136 @@ class Parser:
     def __init__(self, path: str = ""):
         self.path: str = path
 
-    def add_record(self, record: list[str], year: int):
+    def addRecord(self, record: list[str], year: int):
 
         if len(record) == 0:
             return
-        datetime_var = None
+
+        # extracting date
         date = None
+
+        date_string = re.search(r"[A-Z]+[0-9]+", record[0])
+
+        if date_string:
+            datetimeVar = datetime.datetime.strptime(date_string.group(), "%b%d")
+            date = datetime.datetime(year, datetimeVar.month, datetimeVar.day)
+
+        # extracting date and time, if event has time
 
         date_string = re.search(r"[A-Z]+[0-9]+\s+\d+\.\d+", record[0])
 
         if date_string:
-            datetime_var = datetime.datetime.strptime(date_string.group(), "%b%d %H%M%S.%f")
+            datetimeVar = datetime.datetime.strptime(date_string.group(), "%b%d %H%M%S.%f")
             date = datetime.datetime(year,
-                                     datetime_var.month,
-                                     datetime_var.day,
-                                     datetime_var.hour,
-                                     datetime_var.minute,
-                                     datetime_var.second,
-                                     datetime_var.microsecond)
+                                     datetimeVar.month,
+                                     datetimeVar.day,
+                                     datetimeVar.hour,
+                                     datetimeVar.minute,
+                                     datetimeVar.second,
+                                     datetimeVar.microsecond)
+
+        # extracting country
 
         country = None
         test = re.search(r"\.\d+\s+(.+)\(\d+\)", record[0])
         if test:
-            country = re.sub("[.\d\(\)]", "", test.group()).strip()
+            country = re.sub(r"[.\d\(\)]", "", test.group()).strip()
+
+        # extracting latitude and longitude
 
         lat = None
-        lot = None
+        lon = None
 
         if re.search(r"\d+.\d+N", record[0]):
             lat = float(re.search(r"\d+.\d+N", record[0]).group().replace("N", ""))
 
         if re.search(r"\d+.\d+E", record[0]):
-            lot = float(re.search(r"\d+.\d+E", record[0]).group().replace("E", ""))
+            lon = float(re.search(r"\d+.\d+E", record[0]).group().replace("E", ""))
 
-        location_id = None
+        # extracting locationID
+        locationID = None
+        locationMatch = re.search(r"\((\d+)\)", record[0])
 
-        if re.search(r"\((\d+)\)", record[0]):
-            location_id = int(re.search(r"\((\d+)\)", record[0]).group().replace("(", "").replace(")", ""))
+        if locationMatch:
+            locationID = int(re.sub(r"[()]", "", locationMatch.group()))
 
-        record_dictionary = {"time": date, "lat": lat, "lot": lot, "country": country, "location_id": location_id}
+        # creating DataFrame with information about stations
 
-        self._records.append(record_dictionary)
-        print(record_dictionary)
+        # checking if line is in correct format and adding it to queue
+        stationInfoQueue = []
+        for line in record:
+
+            lineMatch = re.search(r"[A-Z]+\s+e(Pg)|(Sg)+\s+\d+\.\d+", line.strip())
+            if lineMatch:
+                stationInfoQueue.append(line.strip())
+
+        # creating table
+        staionNameList = []
+        stationPgList = []
+        stationSgList = []
+        for station in stationInfoQueue:
+
+            # extracting station name
+
+            stationName = None
+            stationNameMatch = re.search(r"^[A-Z]+", station)
+            if stationNameMatch:
+                stationName = stationNameMatch.group()
+                staionNameList.append(stationName)
+
+            # extracting Pg
+            stationPgTimeMatch = re.search(r"ePg\s+\d+\.\d+", station)
+
+            if stationPgTimeMatch:
+                timeText = re.sub(r"[ePg]", "", stationPgTimeMatch.group()).strip()
+                stationPgTimeVar = datetime.datetime.strptime(timeText, "%H%M%S.%f")
+
+                stationPgTime = datetime.datetime(year,
+                                                  date.month,
+                                                  date.day,
+                                                  stationPgTimeVar.hour,
+                                                  stationPgTimeVar.minute,
+                                                  stationPgTimeVar.second,
+                                                  stationPgTimeVar.microsecond)
+                stationPgList.append(stationPgTime)
+            else:
+                stationPgList.append(None)
+
+            # extracting Sg
+            stationSgTimeMatch = re.search(r"eSg\s+\d+\.\d+", station)
+
+            if stationSgTimeMatch:
+                timeText = re.sub(r"[eSg]", "", stationSgTimeMatch.group()).strip()
+                stationSgTimeVar = datetime.datetime.strptime(timeText, "%H%M%S.%f")
+
+                stationSgTime = datetime.datetime(year,
+                                                  date.month,
+                                                  date.day,
+                                                  stationSgTimeVar.hour,
+                                                  stationSgTimeVar.minute,
+                                                  stationSgTimeVar.second,
+                                                  stationSgTimeVar.microsecond)
+                stationSgList.append(stationSgTime)
+            else:
+                stationSgList.append(None)
+
+        # creating dictionary and DataFrame and adding record to a record list
+
+        arrivalTimes = {
+            "station" : staionNameList,
+            "pg_arrival_time" : stationPgList,
+            "sg_arrival_time" : stationSgList
+        }
+        stationDataFrame = pandas.DataFrame(arrivalTimes)
+
+        recordDictionary = {"time": date,
+                            "lat": lat,
+                            "lon": lon,
+                            "country": country,
+                            "location_id": locationID,
+                            "arrival_times": stationDataFrame}
+
+        self._records.append(recordDictionary)
 
     def parse(self):
         file = open(self.path, "r")
@@ -83,16 +158,14 @@ class Parser:
         record = []
 
         for line in file:
-            if line == "Copyright (c) Institute of Geophysics CAS Prague":
+            if line == "Copyright (c) Institute of Geophysics CAS Prague\n":
                 break
             if line == "\n":
-                self.add_record(record, year)
+                self.addRecord(record, year)
                 record = []
                 continue
 
             record.append(line)
-
-        print(record)
 
 
 pars = Parser("2023_02_bul.txt")
