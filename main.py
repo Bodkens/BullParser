f6fab21c8d7b056db00fd0b77575d3e6f988f8f9
bohdan <bogdanreznikov04@gmail.com>
2023-02-21 20:17:54 +0100

    Now program checks each line only one time, records added dynamicly when reading a file

--- main.py
+++ main.py
@@ -14,34 +14,80 @@ class Parser:
     def __init__(self, path: str = ''):
         self.path: str = path
 
-    def addRecord(self, record: list[str], year: int):
+    def parse(self):
+        file = open(self.path, 'r')
 
-        if len(record) == 0:
-            return
+        for i in range(4):
+            file.readline()
 
-        # checking if record having suitable station info and adding it, if record does not have any, we will not add it
-        station_info_queue = []
+        # extracting year of a file
+        year = int(re.search(r'\b\d{4}\b', file.readline()).group())
+
+        file.readline()
+        file.readline()
+
         station_name_list = []
         station_pg_list = []
         station_sg_list = []
         correct_station_lines_present = False
-        for line in record:
+        date = None
+        country = None
+        lat = None
+        lon = None
+        location_id = None
+        for file_line in file:
+            line = file_line.strip()
+            if line == 'Copyright (c) Institute of Geophysics CAS Prague\n':
+                break
+            # checking if it is first line of record
 
-            line_match = re.search(r'[A-Z]+\s+e(Pg)|(Sg)+\s+\d+\.\d+', line.strip())
-            if line_match:
-                correct_station_lines_present = True
-                station = line.strip()
-                date = None
+            if re.search(r'^[A-Z]+[0-9]+(.+)\(\d+\)', line):
 
-                # extracting month and day from first line
-                date_string = re.search(r'[A-Z]+[0-9]+', record[0])
-                station_info_queue.append(station.strip())
+                date_string = re.search(r'[A-Z]+[0-9]+', line)
+
                 if date_string:
                     datetime_var = datetime.datetime.strptime(date_string.group(), '%b%d')
                     date = datetime.datetime(year, datetime_var.month, datetime_var.day)
 
+                # extracting date and time, if event has time
+
+                date_string = re.search(r'[A-Z]+[0-9]+\s+\d+\.\d+', line)
+
+                if date_string:
+                    datetime_var = datetime.datetime.strptime(date_string.group(), '%b%d %H%M%S.%f')
+                    date = datetime.datetime(year,
+                                             datetime_var.month,
+                                             datetime_var.day,
+                                             datetime_var.hour,
+                                             datetime_var.minute,
+                                             datetime_var.second,
+                                             datetime_var.microsecond)
+
+                # extracting country
+                country_match = re.search(r'\.\d+\s+(.+)\(\d+\)', line)
+                if country_match:
+                    country = re.sub(r'[.\d()]', '', country_match.group()).strip()
+
+                # extracting latitude and longitude
+
+                if re.search(r'\d+.\d+N', line):
+                    lat = float(re.search(r'\d+.\d+N', line).group().replace('N', ''))
+
+                if re.search(r'\d+.\d+E', line):
+                    lon = float(re.search(r'\d+.\d+E', line).group().replace('E', ''))
+
+                # extracting location_id
+                location_match = re.search(r'\((\d+)\)', line)
+
+                if location_match:
+                    location_id = int(location_match.group().replace('(', '').replace(')', ''))
+
+            # checking if it is line about stations
+
+            if re.search(r'[A-Z]+\s+e(Pg)|(Sg)+\s+\d+\.\d+', line):
+                correct_station_lines_present = True
                 # extracting station name
-                station_name_match = re.search(r'^[A-Z]+', station)
+                station_name_match = re.search(r'^[A-Z]+', line)
                 if station_name_match:
                     station_name = station_name_match.group()
                     station_name_list.append(station_name)
@@ -49,7 +95,7 @@ class Parser:
                     station_name_list.append(None)
 
                 # extracting Pg
-                station_pg_time_match = re.search(r'ePg\s+\d+\.\d+', station)
+                station_pg_time_match = re.search(r'ePg\s+\d+\.\d+', line)
 
                 if station_pg_time_match and date is not None:
                     time_text = station_pg_time_match.group().replace('ePg', '').strip()
@@ -67,7 +113,7 @@ class Parser:
                     station_pg_list.append(None)
 
                 # extracting Sg
-                station_sg_time_match = re.search(r'eSg\s+\d+\.\d+', station)
+                station_sg_time_match = re.search(r'eSg\s+\d+\.\d+', line)
 
                 if station_sg_time_match and date is not None:
 
@@ -85,101 +131,36 @@ class Parser:
                 else:
                     station_sg_list.append(None)
 
-        if not correct_station_lines_present:
-            return
+            # adding record to list if it has lines with correct format
+            if file_line == '\n':
+                if correct_station_lines_present:
+                    arrival_times = {
+                        'station': station_name_list,
+                        'pg_arrival_time': station_pg_list,
+                        'sg_arrival_time': station_sg_list
+                    }
+                    station_data_frame = pandas.DataFrame(arrival_times)
+                    record_dictionary = {
+                        'time': date,
+                        'lat': lat,
+                        'lon': lon,
+                        'country': country,
+                        'location_id': location_id,
+                        'arrival_times': station_data_frame}
 
-        # extracting date
-        date = None
+                    self._records.append(record_dictionary)
 
-        date_string = re.search(r'[A-Z]+[0-9]+', record[0])
+                station_name_list = []
+                station_pg_list = []
+                station_sg_list = []
+                correct_station_lines_present = False
+                date = None
+                country = None
+                lat = None
+                lon = None
+                location_id = None
 
-        if date_string:
-            datetime_var = datetime.datetime.strptime(date_string.group(), '%b%d')
-            date = datetime.datetime(year, datetime_var.month, datetime_var.day)
-
-        # extracting date and time, if event has time
-
-        date_string = re.search(r'[A-Z]+[0-9]+\s+\d+\.\d+', record[0])
-
-        if date_string:
-            datetime_var = datetime.datetime.strptime(date_string.group(), '%b%d %H%M%S.%f')
-            date = datetime.datetime(year,
-                                     datetime_var.month,
-                                     datetime_var.day,
-                                     datetime_var.hour,
-                                     datetime_var.minute,
-                                     datetime_var.second,
-                                     datetime_var.microsecond)
-
-        # extracting country
-
-        country = None
-        test = re.search(r'\.\d+\s+(.+)\(\d+\)', record[0])
-        if test:
-            country = re.sub(r'[.\d()]', '', test.group()).strip()
-
-        # extracting latitude and longitude
-
-        lat = None
-        lon = None
-
-        if re.search(r'\d+.\d+N', record[0]):
-            lat = float(re.search(r'\d+.\d+N', record[0]).group().replace('N', ''))
-
-        if re.search(r'\d+.\d+E', record[0]):
-            lon = float(re.search(r'\d+.\d+E', record[0]).group().replace('E', ''))
-
-        # extracting location_id
-        location_id = None
-        location_match = re.search(r'\((\d+)\)', record[0])
-
-        if location_match:
-            location_id = int(location_match.group().replace('(', '').replace(')', ''))
-
-        # creating DataFrame with information about stations
-
-        # creating table
-
-        # creating dictionary and DataFrame and adding record to a record list
-
-        arrival_times = {
-            'station': station_name_list,
-            'pg_arrival_time': station_pg_list,
-            'sg_arrival_time': station_sg_list
-        }
-        station_data_frame = pandas.DataFrame(arrival_times)
-        if not station_data_frame.empty:
-            record_dictionary = {'time': date,
-                                'lat': lat,
-                                'lon': lon,
-                                'country': country,
-                                'location_id': location_id,
-                                'arrival_times': station_data_frame}
-
-            self._records.append(record_dictionary)
-
-    def parse(self):
-        file = open(self.path, 'r')
-
-        for i in range(4):
-            file.readline()
-
-        # extracting year of a file
-        year = int(re.search(r'\b\d{4}\b', file.readline()).group())
-
-        file.readline()
-        file.readline()
-        record = []
-
-        for line in file:
-            if line == 'Copyright (c) Institute of Geophysics CAS Prague\n':
-                break
-            if line == '\n':
-                self.addRecord(record, year)
-                record = []
-                continue
-
-            record.append(line)
+        file.close()
 
 
 if __name__ == '__main__':
@@ -187,3 +168,4 @@ if __name__ == '__main__':
     pars.parse()
     for rec in pars.records:
         print(rec)
+
